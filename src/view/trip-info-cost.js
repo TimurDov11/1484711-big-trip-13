cabdcac904765a66c7aed1f7667ddc298cc1b44f
TimurDov11 <65161688+TimurDov11@users.noreply.github.com>
2020-12-08 22:24:12 +0500

    Дополняет подсчёт стоимости предложений

--- src/view/trip-info-cost.js
+++ src/view/trip-info-cost.js
@@ -1,7 +1,21 @@
 export const createTripInfoCostTemplate = (waypoints) => {
+  const createOffers = waypoints.map((waypoint) => {
+    return waypoint.offers;
+  });
+
+  let sum = 0;
+
+  for (let offer of createOffers) {
+    offer.map((value) => {
+      sum = sum + value.price;
+
+      return sum;
+    });
+  }
+
   const calculateTotalPrice = waypoints.reduce((accumulator, waypoint) => {
     return accumulator + waypoint.price;
-  }, 0);
+  }, sum);
 
   return `<p class="trip-info__cost">
       Total: &euro;&nbsp;<span class="trip-info__cost-value">${calculateTotalPrice}</span>
