9d823ea406ca4371be3867a2efe7efed15b01f77
TimurDov11 <65161688+TimurDov11@users.noreply.github.com>
2020-12-26 17:45:41 +0500

    Унаследует простые компоненты

--- src/view/trip-menu.js
+++ src/view/trip-menu.js
@@ -1,27 +1,11 @@
-import {createElement} from "../utils.js";
+import AbstractView from "./abstract.js";
 
 const createTripMenuTemplate = () => {
   return `<nav class="trip-controls__trip-tabs trip-tabs"><a class="trip-tabs__btn" href="#">Table</a><a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Stats</a></nav>`;
 };
 
-export default class TripMenu {
-  constructor() {
-    this._element = null;
-  }
-
+export default class TripMenu extends AbstractView {
   getTemplate() {
     return createTripMenuTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
