7d89d8cf67584239b2aeb3850844d00097ba34ab
TimurDov11 <65161688+TimurDov11@users.noreply.github.com>
2021-01-03 00:24:07 +0500

    Удалет лишнее

--- src/presenter/Trip.js
+++ src/presenter/Trip.js
@@ -1,7 +1,5 @@
 import TripInfoView from "../view/trip-info.js";
 import TripInfoCostView from "../view/trip-info-cost.js";
-//  import SiteMenuView from "../view/trip-menu.js";
-//  import TripFilterView from "../view/trip-filter.js";
 import TripSortView from "../view/trip-sort.js";
 import TripEventsListView from "../view/trip-events-list.js";
 import NoTripEventsItemView from "../view/no-trip-events-item.js";
@@ -9,16 +7,12 @@ import FormEditPointView from "../view/form-edit-point.js";
 import TripEventsItemView from "../view/trip-events-item.js";
 import {render, RenderPosition, replace, remove, EVENT_COUNT, tripEventsElement} from "../utils/render.js";
 
-// const tripEventsElement = pageBodyElement.querySelector(`.trip-events`);
-
 export default class Trip {
   constructor(tripContainer, tripPoints) {
     this._tripContainer = tripContainer;
 
     this._tripInfoComponent = new TripInfoView(tripPoints);
     this._tripInfoCostComponent = new TripInfoCostView(tripPoints);
-    //  this._tripMenuComponent = new TripMenuView();
-    //  this._tripFilterComponent = new TripFilterView();
     this._noTripEventsItemComponent = new NoTripEventsItemView();
     this._tripSortComponent = new TripSortView();
     this._tripEventsListComponent = new TripEventsListView();
@@ -26,12 +20,6 @@ export default class Trip {
 
   init(tripPoints) {
     this._tripPoints = tripPoints.slice();
-    // Метод для инициализации (начала работы) модуля,
-    // малая часть текущей функции renderBoard в main.js
-
-
-    //render(switchTripViewElement, this._tripMenuComponent, RenderPosition.AFTER);
-    //render(tripControlsElement, this._tripFilterComponent, RenderPosition.BEFOREEND);
 
     render(this._tripContainer, this._tripInfoComponent, RenderPosition.AFTERBEGIN);
     render(this._tripInfoComponent, this._tripInfoCostComponent, RenderPosition.BEFOREEND);
@@ -42,9 +30,6 @@ export default class Trip {
 
 
   _renderEventPoint(waypoint) {
-    // Метод, куда уйдёт логика созданию и рендерингу компонетов задачи,
-    // текущая функция renderTask в main.js
-
     const waypointComponent = new TripEventsItemView(waypoint);
     const waypointEditComponent = new FormEditPointView(waypoint);
 
@@ -95,23 +80,16 @@ export default class Trip {
   }
 
   _renderEventPoints() {
-    // Метод для рендеринга N-задач за раз
     for (let i = 0; i < Math.min(this._tripPoints.length, EVENT_COUNT); i++) {
       this._renderEventPoint(this._tripPoints[i]);
     }
   }
 
   _renderNoEventPoints() {
-    // Метод для рендеринга заглушки
     render(tripEventsElement, this._noTripEventsItemComponent, RenderPosition.BEFOREEND);
   }
 
   _renderTrip() {
-    // Метод для инициализации (начала работы) модуля,
-    // бОльшая часть текущей функции renderBoard в main.js
-    //  const tripEventsElement = pageBodyElement.querySelector(`.trip-events`);
-
-
     if (this._tripPoints.length === 0) {
       remove(this._tripInfoComponent);
       remove(this._tripInfoCostComponent);
