2de035fade22e79fb253927b9b563200674cec81
TimurDov11 <65161688+TimurDov11@users.noreply.github.com>
2021-01-27 00:00:36 +0500

    Устанавливает и импортирует пакет и плагин, Вставляет код

--- src/view/trip-stats.js
+++ src/view/trip-stats.js
@@ -1,12 +1,149 @@
-import AbstractView from "./abstract.js";
+import SmartView from "./smart.js";
+import Chart from "chart.js";
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 //  import {MenuItem} from "../const.js";
+
+// Рассчитаем высоту канваса в зависимости от того, сколько данных в него будет передаваться
+const moneyCtx = document.querySelector(`.statistics__chart--money`);
+const typeCtx = document.querySelector(`.statistics__chart--transport`);
+//const timeCtx = document.querySelector(`.statistics__chart--time`);
+
+const moneyChart = new Chart(moneyCtx, {
+  plugins: [ChartDataLabels],
+  type: `horizontalBar`,
+  data: {
+    labels: [`TAXI`, `BUS`, `TRAIN`, `SHIP`, `TRANSPORT`, `DRIVE`],
+    datasets: [{
+      data: [400, 300, 200, 160, 150, 100],
+      backgroundColor: `#ffffff`,
+      hoverBackgroundColor: `#ffffff`,
+      anchor: `start`
+    }]
+  },
+  options: {
+    plugins: {
+      datalabels: {
+        font: {
+          size: 13
+        },
+        color: `#000000`,
+        anchor: `end`,
+        align: `start`,
+        formatter: (val) => `€ ${val}`
+      }
+    },
+    title: {
+      display: true,
+      text: `MONEY`,
+      fontColor: `#000000`,
+      fontSize: 23,
+      position: `left`
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          fontColor: `#000000`,
+          padding: 5,
+          fontSize: 13,
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        barThickness: 44,
+      }],
+      xAxes: [{
+        ticks: {
+          display: false,
+          beginAtZero: true,
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        minBarLength: 50
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      enabled: false,
+    }
+  }
+});
+
+const typeChart = new Chart(typeCtx, {
+  plugins: [ChartDataLabels],
+  type: `horizontalBar`,
+  data: {
+    labels: [`TAXI`, `BUS`, `TRAIN`, `SHIP`, `TRANSPORT`, `DRIVE`],
+    datasets: [{
+      data: [4, 3, 2, 1, 1, 1],
+      backgroundColor: `#ffffff`,
+      hoverBackgroundColor: `#ffffff`,
+      anchor: `start`
+    }]
+  },
+  options: {
+    plugins: {
+      datalabels: {
+        font: {
+          size: 13
+        },
+        color: `#000000`,
+        anchor: `end`,
+        align: `start`,
+        formatter: (val) => `${val}x`
+      }
+    },
+    title: {
+      display: true,
+      text: `TYPE`,
+      fontColor: `#000000`,
+      fontSize: 23,
+      position: `left`
+    },
+    scales: {
+      yAxes: [{
+        ticks: {
+          fontColor: `#000000`,
+          padding: 5,
+          fontSize: 13,
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        barThickness: 44,
+      }],
+      xAxes: [{
+        ticks: {
+          display: false,
+          beginAtZero: true,
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        minBarLength: 50
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      enabled: false,
+    }
+  }
+});
 
 const createTripStatsTemplate = () => {
   return `<section class="statistics">
     <h2 class="visually-hidden">Trip statistics</h2>
 
     <div class="statistics__item statistics__item--money">
-      <canvas class="statistics__chart  statistics__chart--money" width="900"></canvas>
+      <canvas class="statistics__chart  statistics__chart--money" height="" width="900"></canvas>
     </div>
 
     <div class="statistics__item statistics__item--transport">
@@ -19,8 +156,35 @@ const createTripStatsTemplate = () => {
   </section>`;
 };
 
-export default class TripStats extends AbstractView {
+export default class TripStats extends SmartView {
+  constructor(tasks) {
+    super();
+
+    this._moneyChart = null;
+    this._typeChart = null;
+    //this._timeChart = null;
+
+    this._setCharts();
+  }
+
   getTemplate() {
     return createTripStatsTemplate();
+  }
+
+  _setCharts() {
+    if (this._moneyChart !== null || this._typeChart !== null || this._timeChart !== null) {
+      this._moneyChart = null;
+      this._typeChart = null;
+      //  this._timeChart = null;
+    }
+
+    //  const {tasks, dateFrom, dateTo} = this._data;
+    const BAR_HEIGHT = 55;
+    moneyCtx.height = BAR_HEIGHT * 5;
+    typeCtx.height = BAR_HEIGHT * 5;
+    //  timeCtx.height = BAR_HEIGHT * 5;
+
+    this._moneyChart = moneyChart;
+    this._typeChart = typeChart;
   }
 }
